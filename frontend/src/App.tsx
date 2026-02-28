e38b8f2b91702da13d58a98a2495d11ad0d6dee5
Hitesh Navare <navarehitesh65@gmail.com>
2026-02-28 21:39:17 +0530

    added loginRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import DashBoard from "./Components/DashboardComponents/DashBoard"
+import LandingPage from "./Components/DashboardComponents/LandingPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { SignupForm } from "./Components/DashboardComponents/SignupForm"
+import { LoginForm } from "./Components/DashboardComponents/LoginForm"
 
-function App() {
-
+export default function App() {
   return (
-    <>
-     Hello
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/signup' element={<SignupForm />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/dashboard' element={<DashBoard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
-
-export default App
