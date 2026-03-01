151c1698e07a70cefc6098285126de01f4aabb12
Hitesh Navare <navarehitesh65@gmail.com>
2026-03-01 19:35:17 +0530

    added the questionpage boiler template

---
+++ frontend/src/Components/DashboardComponents/QuestionPage.tsx
@@ -0,0 +1,11 @@
+
+export default function QuestionPage() {
+  return (
+    <div className="max-w-[50vw] mx-auto bg-red-400 h-screen w-screen ">
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
